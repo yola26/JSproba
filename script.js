e09d0f5a4a9028fa6154fd7f554a5b27770b17ab
Yola <elenaweb2605@gmail.com>
2022-06-13 14:08:36 +0300

    add if, else, switch

--- script.js
+++ script.js
@@ -19,3 +19,35 @@ personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
+
+const num = 50;
+
+if (num < 49) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('Много');
+} else {
+    console.log('OK!');
+}
+
+const num = 50;
+(num === 50) ? console.log('OK!') : console.log('Error');
+
+
+
+const num = 50;
+
+switch (num) {
+    case 49:
+        console.log('Неверно');
+        break;
+    case 100:
+        console.log('Неверно');
+        break;
+    case 50:
+        console.log('В точку!');
+        break;
+    default:
+        console.log('Не в этот раз!');
+        break;
+};
