e7ab1965588018e3cb7739ed2e79c8ccecfd03b3
Yola <elenaweb2605@gmail.com>
2022-07-06 13:30:28 +0300

    exesizes 2

--- script.js
+++ script.js
@@ -1,75 +1,53 @@
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
 'use strict';
 
-for (let i = 0; i < 3; i++) {
-    console.log (i);
-}
+const numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?');
 
-let result = '';
-const lenght = 7;
-
-for (let i =1; i < lenght; i++) {
-
-    for (let j = 0; j < i; j++) {
-        result += "*";
-    }
-    result += '\n';
-}
-
-console.log(result);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
 
-
-for (let i = 0; i < 3; i++) {
-    console.log ('First level: ${i}');
-    for (let j = 0; j < 3; j++) {
-        console.log ('Second level: ${j}');
-        for (let k = 0; k < 3; k++) {
-            console.log ('Third level: ${k}');
-        }
-    }
-}
-
-
-
-
-for (let i = 5; i < 11; i++) {
-    console.log (i);
-}
-
-
-for (let i = 20; i > 9; i--) {
-    if (i ===13) {
-        break;
-    }
-    console.log (i);
-}
-
-
-for (let i = 2; i <=10; i++) {
-    if (i % 2 === 0) {
-      console.log (i);
-    }  
-}
-
-
-for (let i = 2; i <= 16; i++) {
-
-    if (i % 2 === 0) {
-        continue;
-    }
-    console.log(i);
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
     
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
 }
 
 
-let i = 2;
+console.log(personalMovieDB);
 
-while (i <= 16) {
-    if (i % 2 === 0) {
-        i++;
-        continue;
-    } else {
-        console.log(i); 
-    }
-    i++;
-} 
